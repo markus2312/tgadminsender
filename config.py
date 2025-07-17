6c35d5cd6fcaea7d46a798faca76a8c3f8bfb1a4
markus2312 <96542767+markus2312@users.noreply.github.com>
2025-07-17 12:01:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-API_ID = '6066608'
-API_HASH = '17de6fd39b8f10221248297b90c8446b'
-SESSION_NAME = 'bot_session'
+import os
 
-GOOGLE_SHEET_NAME = 'КАДРОФФ TG реакции бот'
+# Получаем из переменных окружения
+API_ID = int(os.getenv("TG_API_ID", 0))
+API_HASH = os.getenv("TG_API_HASH", "")
+SESSION_NAME = os.getenv("TG_SESSION_NAME", "79398382811")  # по умолчанию имя файла сессии без .session
