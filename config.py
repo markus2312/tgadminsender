2e0c5a2e5b6fa94571fa2e617c66121cada4ca14
markus2312 <96542767+markus2312@users.noreply.github.com>
2025-07-17 10:41:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-API_ID = 'your_api_id'
-API_HASH = 'your_api_hash'
+API_ID = '6066608'
+API_HASH = '17de6fd39b8f10221248297b90c8446b'
 SESSION_NAME = 'bot_session'
 
 GOOGLE_SHEET_NAME = 'КАДРОФФ TG реакции бот'
